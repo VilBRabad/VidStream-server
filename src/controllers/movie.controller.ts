826460eeb184beaa1b2026399d2a8ece0b2d6749
VilBRabad <rabadvilas11@gmail.com>
2024-08-11 21:12:50 +0530

    Added movies filters

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -3,8 +3,9 @@ import { asyncHandler } from "../utils/AsyncHandler";
 import { Movie } from "../models/movie.model";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
+import mongoose from "mongoose";
 
-const getHomePageMovieInfo = asyncHandler(async(req: Request, res: Response)=>{
+const getHomePageMovieInfo = asyncHandler(async (req: Request, res: Response) => {
     const topFiveMovie = await Movie.aggregate([
         {
             $sort: {
@@ -34,7 +35,7 @@ const getHomePageMovieInfo = asyncHandler(async(req: Request, res: Response)=>{
         {
             $match: {
                 genre: {
-                    $elemMatch: {$eq: "Romance"}
+                    $elemMatch: { $eq: "Romance" }
                 }
             }
         },
@@ -55,7 +56,7 @@ const getHomePageMovieInfo = asyncHandler(async(req: Request, res: Response)=>{
     }
 
 
-    if(!movie && !topFiveMovie && !topRommance) throw new ApiError(400, "Movies not found");
+    if (!movie && !topFiveMovie && !topRommance) throw new ApiError(400, "Movies not found");
 
     return res.json(
         new ApiResponse(200, movieData, "found successfully")
@@ -63,4 +64,86 @@ const getHomePageMovieInfo = asyncHandler(async(req: Request, res: Response)=>{
 })
 
 
-export {getHomePageMovieInfo};
+const getMovieById = asyncHandler(async (req: Request, res: Response) => {
+    const { movieId } = req.query;
+
+    if (typeof movieId !== 'string' || !mongoose.Types.ObjectId.isValid(movieId)) {
+        throw new ApiError(400, "Invalid id");
+    }
+
+    const movie = await Movie.findOne({ _id: movieId });
+
+    if (!movie) throw new ApiError(402, "Movie not found");
+
+    return res.json(
+        new ApiResponse(200, movie)
+    );
+})
+
+
+const getMoviesByGenreFilters = asyncHandler(async (req: Request, res: Response)=>{
+    const {genres, page='1', limit='10'} = req.query;
+
+    const genreArray: string[] = typeof genres === 'string' ? genres.split(","): [];
+    const pageNumber = parseInt(page as string, 10);
+    const limitNumber = parseInt(limit as string, 10);
+
+    // console.log(genreArray, genres);
+
+    const movies = await Movie.aggregate([
+        {
+            $match: genres? {
+                genre: {
+                    $in: genreArray
+                }
+            }: {}
+        },
+        {
+            $facet: {
+                metaData: [{$count: 'total'}],
+                data: [
+                    { $skip: (pageNumber-1)*limitNumber },
+                    { $limit: limitNumber },
+                    { $sort: {rating: -1} }
+                ]
+            }
+        },
+        {
+            $project: {
+                totalPages: {
+                    $ceil: {
+                        $divide: [
+                            {$arrayElemAt: ['$metaData.total', 0]},
+                            limitNumber
+                        ]
+                    }
+                },
+                totalMovies: { $arrayElemAt: ["$metaData.total", 0] },
+                movies: '$data'
+            }
+        }
+    ]);
+
+    const moviesData = {currentPage: pageNumber, ...movies[0]};
+    console.log(moviesData);
+    res.status(200).json(
+        new ApiResponse(200, moviesData)
+    );
+})
+
+
+const testUrl = asyncHandler(async (req: Request, res: Response) => {
+    const { vi } = req.query;
+    console.log(vi);
+
+    return res.json(
+        new ApiResponse(200, { vi }, "res")
+    );
+})
+
+export {
+    getHomePageMovieInfo,
+    getMovieById,
+    getMoviesByGenreFilters,
+    testUrl
+};
--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import {getHomePageMovieInfo} from "../controllers/movie.controller"
+import {
+    getHomePageMovieInfo,
+    getMovieById,
+    getMoviesByGenreFilters,
+    testUrl
+} from "../controllers/movie.controller"
 
 const router = Router();
 
 router.get("/home-movies-info", getHomePageMovieInfo);
+router.get("/get-movie-by-id", getMovieById);
+router.get("/movies", getMoviesByGenreFilters);
+router.get("/test/movie", testUrl);
 
 export default router;
